eeccb991821877b2c3d1f308a1ebf51136978d7a
xuyuan <yyyyyyyoko@163.com>
2025-06-05 22:14:21 +0800

    Update softmax_regression-exercise.py
    添加模型保存功能

--- src/chap03_softmax_regression/softmax_regression-exercise.py
+++ src/chap03_softmax_regression/softmax_regression-exercise.py
@@ -201,6 +201,12 @@ Z = Z.reshape(X.shape)
 plt.contour(X, Y, Z)
 plt.show()
 
+# 保存模型
+model.save_weights('softmax_regression_weights')
+
+# 加载模型
+model.load_weights('softmax_regression_weights')
+
 
 # In[ ]:
 
